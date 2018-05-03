804a3aa1bd8124eddba11728066bb46e27ac829f
jefferyclanton <37197975+jefferyclanton@users.noreply.github.com>
2018-05-03 13:10:28 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './favicon.ico';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar />
+    <div className="App-playlist">
+      <SearchResults />
+      <Playlist />
+    </div>
+  </div>
+</div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+  render() {
+    <div className="SearchResults">
+  <h2>Results</h2>
+  <TrackList />
+</div>
+  }
+}
+
+export default SearchResults;
